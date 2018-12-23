fb2a845a7af55f369f71c822e4bdd8fe57f7ae62
Szymon Bezpalko <szymonbezpalko@gmail.com>
2018-12-23 14:53:42 +0100

    cleaned unneccesary data

--- setup_data.sh
+++ setup_data.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 mkdir -p data
+mkdir -p result_data
+mkdir -p maps
+mkdir -p offline_data
 curl -L --data "@postdata.dat" "https://www.transtats.bts.gov/DownLoad_Table.asp?Table_ID=293&Has_Group=3&Is_Zipped=0" > data/flights.zip
 
 cd data
